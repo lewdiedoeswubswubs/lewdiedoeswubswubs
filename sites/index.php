0078e29ba38ea21777b68bf5554f51efcd02a1c6
lewdiedoeswubswubs <48870163+lewdiedoeswubswubs@users.noreply.github.com>
2022-06-13 15:08:55 +0800

    sds

--- sites/index.php
+++ sites/index.php
@@ -1,3 +1,9 @@
+<?php
+    session_start()
+    include("connection.php")
+    include("functions.php")
+    $user_data = check_login($conn)
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
